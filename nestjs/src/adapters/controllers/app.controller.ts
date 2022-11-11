f32ea016f524772e080b01bddc16d1bd8a2f37c0
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2022-11-11 11:10:40 +0300

    fix runnig program

--- nestjs/src/adapters/controllers/app.controller.ts
+++ nestjs/src/adapters/controllers/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppUseCases } from 'src/use-cases/app/app.use-case';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appUseCases: AppUseCases) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appUseCases.getHello();
   }
 }
--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -4,6 +4,10 @@ import { ThrottlerModule } from '@nestjs/throttler';
 
 import configuration from './configuration/configuration';
 import { AppController } from './adapters/controllers/app.controller';
+import { UserUseCaseModule } from './use-cases/user/user.use-case.module';
+import { AppUseCases } from './use-cases/app/app.use-case';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
@@ -14,8 +18,16 @@ import { AppController } from './adapters/controllers/app.controller';
     ConfigModule.forRoot({
       load: [configuration],
     }),
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    //   installSubscriptionHandlers: true,
+    //   autoSchemaFile: true,
+    //   // autoSchemaFile: join(process.cwd(), "src/schema.gql"),
+    //   sortSchema: true,
+    // }),
+    UserUseCaseModule,
   ],
   controllers: [AppController],
-  providers: [],
+  providers: [AppUseCases],
 })
 export class AppModule {}
